d79947009ce9314e9815830f873e40e505d40577
onahbosen <onahbosen@gmail.com>
2019-02-28 14:34:49 -0600

    add new animal button

--- GifTastic/assets/javascript/app.js
+++ GifTastic/assets/javascript/app.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     var animalArr = ["dog", "cat", "bird", "raccoon", "hedgehog", "skunk", "seal", "bunny"];
 
     function newButton(){
+        $('.btn-group').empty();
         for (var i = 0; i < animalArr.length; i++){
             $('.btn-group').append('<button data-animal="' + animalArr[i] + '"type="button" class="btn btn-light">' + animalArr[i] + '</button>');
         }
@@ -24,10 +25,23 @@ $(document).ready(function(){
             var results = response.data;
 
             for (var i = 0; i < results.length; i++){
+                var gifDiv = $('<div>');
+                var rating = results[i].rating;
+                var p = $('<p>').text("rating: " + rating);
                 var animalImage = $('<img>');
                 animalImage.attr('src', results[i].images.fixed_height.url);
-                $('.gif-space').prepend(animalImage);
+                animalImage.addClass('gif');
+                gifDiv.append(animalImage);
+                gifDiv.append(p);
+                $('.gif-space').prepend(gifDiv);
             }
         })
+    });
+
+    $('.add-animal').on('click', function(e){
+        e.preventDefault();
+        var newAnimal = $('#animal-input').val().trim();
+        animalArr.push(newAnimal);
+        newButton();
     });
 });
